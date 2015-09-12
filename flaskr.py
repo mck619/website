cd0325945076b7f7d3809ef8a8557bbee210c341
Michael Kakehashi <mckakehashi@gmail.com>
2015-09-11 20:13:30 -0400

    just continuing with flaskr tutorial

--- flaskr.py
+++ flaskr.py
@@ -2,13 +2,14 @@
 import psycopg2
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+from contextlib import closing
 
 # configuration
 f = open('config.txt')
 secrets = f.readlines()
 f.close
 DATABASE = '/tmp/flaskr.db'
-DEBUG = False
+DEBUG = True
 SECRET_KEY = secrets[0].replace('\n','')
 USERNAME = secrets[1].replace('\n','')
 PASSWORD = secrets[2].replace('\n','')
@@ -18,17 +19,19 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-
-
+@app.route('/HAL')
+def HAL():
+	return "I'm sorry, Dave. I'm afraid I can't do that."
 
 
 def connect_db():
-	f = open(dbpwd.txt)
-	pwd = f.readline()
+	f = open('dbpwd.txt')
+	pwd = f.readline().replace('\n','')
 	try:
-		conn =  psycopg2.connect(dbname = 'mydb', user = 'postgres', host='localhost', password=pwd)
+		conn =  psycopg2.connect(dbname = 'mydb', user = 'postgres', password = pwd, host='localhost')
 		f.close
-	except:
+	except Exception as e:
+		print(e)
 		f.close
 		print('cant connect to database')
 	return conn
@@ -36,12 +39,28 @@ def connect_db():
 
 def init_db():
 	with closing(connect_db()) as db:
-		with app.open_resource('schema.sql', mode='r') as f:
+		with app.open_resource('dbtester.sql', mode='r') as f:
 			db.cursor().execute(f.read())
-	db.commit()
+		db.commit()
+
+@app.before_request
+def before_request():
+	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+	db = getattr(g, 'db', None)
+	if db is not None:
+		db.close()
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(host='0.0.0.0')
 
 
