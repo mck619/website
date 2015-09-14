5e8a4ef271afbcab127fd3f0f9653d2cf922c4e6
Michael Kakehashi <mckakehashi@gmail.com>
2015-09-14 13:57:31 -0400

    more tutorial

--- flaskr.py
+++ flaskr.py
@@ -4,6 +4,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from contextlib import closing
 
+
 # configuration
 f = open('config.txt')
 secrets = f.readlines()
@@ -53,12 +54,43 @@ def teardown_request(exception):
 	if db is not None:
 		db.close()
 
+@app.route('/')
+def show_entries():
+	cur = g.db.cursor()
+	cur.execute('select title, text from entries order by id desc')
+	entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+	return render_template('show_entries.html', entries=entries)
 
+@app.route('/add', methods=['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	cur = g.db.cursor()
+	cur.execute('insert into entries (title, text) values (%s, %s)',
+                 [request.form['title'], request.form['text']])
+	g.db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
 
-
-
-
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
